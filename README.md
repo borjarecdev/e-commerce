bb9c9e9e232bc7d3f8b12bffaded1d0b14cf8bc9
Borja Recio <borjarec2@gmail.com>
2024-12-23 20:35:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# E-commerce store
+
+El proyecto ha sido desarrollado en [Angular CLI](https://github.com/angular/angular-cli) version 18.0.1.
+
+
+## Authors
+
+- [@borjarecdev](https://www.github.com/borjarecdev)
---
+++ src/app/products/features/product-detail/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, effect, inject, input } from '@angular/core';
+import { ProductDetailStateService } from '../../data-access/proudct-detail-state.service';
+import { CurrencyPipe } from '@angular/common';
+import { CartStateService } from '../../../shared/data-access/cart-state.service';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './product-detail.component.html',
+  providers: [ProductDetailStateService],
+})
+export default class ProductDetailComponent {
+  productDetailState = inject(ProductDetailStateService).state;
+  cartState = inject(CartStateService).state;
+
+  id = input.required<string>();
+
+  constructor() {
+    effect(() => {
+      this.productDetailState.getById(this.id());
+    });
+  }
+
+  addToCart() {
+    this.cartState.add({
+      product: this.productDetailState.product()!,
+      quantity: 1,
+    });
+  }
+}
